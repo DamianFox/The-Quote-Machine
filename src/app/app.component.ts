daef2974e7a45d1ec29c000e3976e38957e07b33
damiano.fossa <d.fossa.91@gmail.com>
2017-12-14 22:29:00 +0100

    File modification for the Routing Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+  title = 'The Quote Machine!';
+  description = 'A new app';
+  query: string;
+  private routeSub:any;
+
+
+  constructor(private route:ActivatedRoute){
+      this.routeSub = route.params.subscribe(params=>{
+          console.log(params)
+          this.query = params['q']
+      })
+  }
+
+  ngOnInit() {
+      
+  }
+  ngOnDestroy(){
+      this.routeSub.unsubscribe()
+  }
 }
+
